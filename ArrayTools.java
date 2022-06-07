80695997dda1153ad3a567b089927abebcf9ef4f
Meghna <meghnapothan@gmail.com>
2022-06-07 19:13:47 -0400

    addeed description

--- ArrayTools.java
+++ ArrayTools.java
@@ -1,4 +1,9 @@
-//Athul Babu
+//Meghna Aravind A00256942
+//Athul Babu A00244116
+//Lab 2 - Ceaser Cipher Encryption and Decryption 
+// Avarage of an array
+// Array search
+//Array reverse
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -14,23 +19,23 @@ public class ArrayTools {
         System.out.println("Enter a value to encrypt with: "); 
         Scanner scanner_two = new Scanner(System.in);  
         shiftValue = scanner_two.nextInt();
-        String encyptedValue=Caesar_cipher(plainText,shiftValue);
+        String encyptedValue=Caesar_cipher(plainText,shiftValue); // Calling encryption function
         System.out.println("The encrypted string is: " +encyptedValue);
         System.out.println("Decrypting with " + -shiftValue +"... \n" +Caesar_cipher(encyptedValue,-shiftValue));
-        int[] arr = {44, 78, 45, 77, 44, 98, 67, 68, 91, 54};
+        int[] arr = {44, 78, 45, 77, 44, 98, 67, 68, 91, 54}; // hardcoded integer array
         System.out.println("Testing methods with " + Arrays.toString(arr));
-        System.out.println("The Average is : " + arrayAvg(arr));
+        System.out.println("The Average is : " + arrayAvg(arr)); // calling arrayavarage function
 
         Scanner scanner_three = new Scanner(System.in);  
        
         System.out.println("Enter a value to search for ");
        int searchValue = scanner_three.nextInt();
-       if(arraySearch(arr,searchValue)){
+       if(arraySearch(arr,searchValue)){  // calling array search function
            System.out.println("The array contains "+searchValue);
        }else{
         System.out.println("The array do not contains "+searchValue);
        }
-       System.out.println("The array reversed is "+Arrays.toString(arrayReverse(arr)));
+       System.out.println("The array reversed is "+Arrays.toString(arrayReverse(arr))); // Calling array reverse function
        
    }
 
@@ -71,11 +76,11 @@ if(searchValue==arr[i]){
         for (int ii = 0; ii < message.length(); ii++) {
           int charPosition = alphabet.indexOf(message.charAt(ii));
        
-           keyVal = ((((shiftValue + charPosition) % 26) + 26) % 26);
+           keyVal = ((((shiftValue + charPosition) % 26) + 26) % 26);// normal mod function was not working in decription using negative value
          
           
-          char replaceVal = alphabet.charAt(keyVal);
-          cipherText += replaceVal;
+          char newValue = alphabet.charAt(keyVal);
+          cipherText += newValue;
         }
         return cipherText;
     }
